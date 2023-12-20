a6e3bee991bf39e4cebb6ca44ff93a964c1ca062
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-20 16:20:02 +0100

    Add - store crud

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', [CharacterController::class, 'index'])->name('characters.index')
 Route::get('/characters/create', [CharacterController::class, 'create'])->name('characters.create');
 
 Route::get('/characters/{character}', [CharacterController::class, 'show'])->name('characters.show');
+
+Route::post('/characters',[CharacterController::class, 'store'])->name('characters.store');
