32e90485aaabbb44abe24bc984483cc5050ef2e4
Nicola Tabai <nicola.tabai98@gmail.com>
2023-12-20 17:15:29 +0100

    Add nav on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,19 @@
 @yield('title')
+
+<nav class="container">
+    <ul class="d-flex justify-content-between">
+        <li>
+            <a href="{{ route('characters.index') }}">Home</a>
+        </li>
+        <li>
+            <a href="#"></a>
+        </li>
+        <li>
+            <a href="#"></a>
+        </li>
+        <li>
+            <a href="#"></a>
+        </li>
+    </ul>
+
+</nav>
