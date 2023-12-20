7501ee68c58c6847a1fd275ed6d8063a70b59b23
aSwanting <gab.damico@gmail.com>
2023-12-20 16:04:00 +0100

    ADD - create view, route

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -4,61 +4,65 @@
 
 @section('content')
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Bio</th>
-            <th>Def</th>
-            <th>Speed</th>
-            <th>HP</th>
-            <th>attack</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($characters as $character)
-        <tr>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->name }}
-                </a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Bio</th>
+                <th>Def</th>
+                <th>Speed</th>
+                <th>HP</th>
+                <th>attack</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($characters as $character)
+                <tr>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->name }}
+                        </a>
 
-            </td>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->bio }}
-                </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->bio }}
+                        </a>
 
-            </td>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->defense }}
-                </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->defense }}
+                        </a>
 
-            </td>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->speed }}
-                </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->speed }}
+                        </a>
 
-            </td>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->hp }}
-                </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->hp }}
+                        </a>
 
-            </td>
-            <td>
-                <a href="{{route('characters.show', $character)}}">
-                    {{ $character->attack }}
-                </a>
-            </td>
-        </tr>
-        @empty
-        Nessun personaggio trovato!
-        @endforelse
-    </tbody>
-</table>
+                    </td>
+                    <td>
+                        <a href="{{ route('characters.show', $character) }}">
+                            {{ $character->attack }}
+                        </a>
+                    </td>
+                </tr>
+            @empty
+                Nessun personaggio trovato!
+            @endforelse
+        </tbody>
+    </table>
+
+    <a href="{{ route('characters.create') }}">
+        <button class="btn btn-outline-primary">Create Character</button>
+    </a>
 
 
 
