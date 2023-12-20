060981f9e5b81117a900ea7c92b2a7ef1056615a
aSwanting <gab.damico@gmail.com>
2023-12-20 20:12:40 +0100

    add - show route styling

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -2,18 +2,16 @@
 @section('title', 'show-character')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-4">
-            <ul>
-                <li>{{$character->name}}</li>
-                <li>{{$character->bio}}</li>
-                <li>{{$character->defense}}</li>
-                <li>{{$character->hp}}</li>
-                <li>{{$character->attack}}</li>
-                <li>{{$character->speed}}</li>
-            </ul>
-        </div>
+    <div class="container">
+
+        <h2 class="text-center display-2 mb-5">{{ $character->name }}</h2 class="display -2">
+        <p class="display-6 mb-5">{{ $character->bio }}</p>
+        <ul class="display-6 list-unstyled d-flex justify-content-center gap-5">
+            <li><span class="fw-bold">HP</span> {{ $character->hp }}</li>
+            <li><span class="fw-bold">ATK</span> {{ $character->attack }}</li>
+            <li><span class="fw-bold">DEF</span> {{ $character->defense }}</li>
+            <li> <span class="fw-bold">SPD</span> {{ $character->speed }}</li>
+        </ul>
+
     </div>
-</div>
 @endsection
