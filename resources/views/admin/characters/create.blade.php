8413435c1ce69ea927b386218ee0dcb730fd1525
aSwanting <gab.damico@gmail.com>
2024-01-11 16:04:45 +0100

    add: admin folder, fixed routes

--- resources/views/admin/characters/create.blade.php
+++ resources/views/admin/characters/create.blade.php
@@ -1,48 +1,44 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Character')
+@section('title', 'Create Character')
 
 @section('content')
     <div class="container rounded rounded-4 p-5 shadow bg-white">
 
-        <form action="{{ route('characters.update', $character) }}" method="POST">
-
+        <form action="{{ route('admin.characters.store') }}" method="POST">
             @csrf
-
-            @method('PUT')
-
             <div class="mb-3">
                 <label for="name">Character Name</label>
-                <input class="form-control" type="text" name="name" id="name" required value="{{ $character->name }}">
+                <input class="form-control" type="text" name="name" id="name" required>
             </div>
 
             <div class="mb-3">
                 <label for="hp">HP</label>
                 <input class="form-control" type="number" name="hp" id="hp" min="0" max="9999"
-                    required value="{{ $character->hp }}">
+                    required>
             </div>
 
             <div class="mb-3">
                 <label for="attack">Attack</label>
                 <input class="form-control" type="number" name="attack" id="attack" min="0" max="999"
-                    required value="{{ $character->attack }}">
+                    required>
             </div>
 
             <div class="mb-3">
                 <label for="defense">Defense</label>
                 <input class="form-control" type="number" name="defense" id="defense" min="0" max="999"
-                    required value="{{ $character->defense }}">
+                    required>
             </div>
 
             <div class="mb-3">
                 <label for="speed">Speed</label>
                 <input class="form-control" type="number" name="speed" id="speed" min="0" max="999"
-                    required value="{{ $character->speed }}">
+                    required>
             </div>
 
             <div class="mb-5">
                 <label for="bio">Character Bio</label>
-                <textarea class="form-control" name="bio" id="bio" rows="2">{{ $character->bio }}</textarea>
+                <textarea class="form-control" name="bio" id="bio" rows="2"></textarea>
             </div>
 
             <div class="d-flex gap-3">
