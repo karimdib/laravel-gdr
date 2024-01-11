8eb63b39391b268ab3be8fa251db1de17e48c8c1
ValerioCarbone <valeriocarbone10@gmail.com>
2024-01-11 18:09:22 +0100

    CHANGE Types Create & Edit

--- resources/views/admin/characters/show.blade.php
+++ resources/views/admin/characters/show.blade.php
@@ -7,6 +7,7 @@
         <h2 class="text-center display-2 mb-5">{{ $character->name }}</h2 class="display -2">
         <p class="display-6 mb-5">{{ $character->bio }}</p>
         <ul class="display-6 list-unstyled d-flex justify-content-center gap-5">
+            <li><span class="fw-bold">TYPE</span> {{ $character->type->name }}</li>
             <li><span class="fw-bold">HP</span> {{ $character->hp }}</li>
             <li><span class="fw-bold">ATK</span> {{ $character->attack }}</li>
             <li><span class="fw-bold">DEF</span> {{ $character->defense }}</li>
