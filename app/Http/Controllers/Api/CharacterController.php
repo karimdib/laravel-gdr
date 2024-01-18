ddb6805def2ab3f43573d9fa838ef5bcc66cc1c4
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-18 15:51:23 +0100

    Add index function in Character Controller

--- app/Http/Controllers/Api/CharacterController.php
+++ app/Http/Controllers/Api/CharacterController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Character;
 
 class CharacterController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $results = Character::with('items', 'type')->paginate(20);
+        // $results = Character::all();
+
+        return response()->json([
+            'results' => $results,
+            'success' => true
+        ]);
+    }
 }
